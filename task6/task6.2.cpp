0ee7d7f7133d99049d4faa7247f2f939a79938f1
fredriknystrom <fredriknystroms@gmail.com>
2022-12-07 10:05:23 +0100

    finished task 6

---
+++ task6/task6.2.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool containsDuplicate(vector<char> chars) {
+	if (!is_sorted(chars.begin(), chars.end()))
+		sort(chars.begin(), chars.end());
+
+	return adjacent_find(chars.begin(), chars.end()) != chars.end();
+}
+
+int pointsFromFile(string fileName){
+
+    ifstream myFileStream(fileName);
+    string line, firstIDS, secondIDS;
+    int position = 0;
+  
+    getline(myFileStream, line);
+
+    vector<char> vector;
+
+    for(int i = 0; i < line.length(); i++){
+        vector.push_back(line[i]);
+        ++position;
+        if (vector.size() > 14){
+            vector.erase(vector.begin());
+        }
+        if (vector.size() == 14 && !containsDuplicate(vector)){
+            return position;
+        }
+
+    }
+    myFileStream.close();
+    return -1;
+}
+
+int main(){
+    string fileName = "tuningTrouble.txt";
+    cout << pointsFromFile(fileName);
+    return 0;
+}
